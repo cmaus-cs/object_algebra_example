80bf6c166091c34d7a511c9d9d32d08db8c7ae7b
Christian Maus <c.maus@reply.de>
2022-05-29 10:43:10 +0200

    create multiplication language as extension of expression language

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,23 +3,32 @@ fun main(args: Array<String>) {
     with(EvalExp()) {
         println(e1().eval())
     }
+    with(EvalMul()) {
+        println(e2().eval())
+    }
 }
 
 
 context(ExpAlg<T>)
 fun <T> e1(): T = lit(1) + lit(2) + lit(3)
 
+context(MulAlg<T>)
+fun <T> e2(): T = lit(5) + lit(6) * lit(4)
 
 internal interface ExpAlg<T> {
     fun lit(n: Int): T
     operator fun T.plus(value: T): T
 }
 
+internal interface MulAlg<T> : ExpAlg<T> {
+    operator fun T.times(value: T): T
+}
+
 internal interface Eval {
     fun eval(): Int
 }
 
-internal class EvalExp : ExpAlg<Eval> {
+internal open class EvalExp : ExpAlg<Eval> {
     override fun lit(n: Int): Eval = object : Eval {
         override fun eval(): Int = n
     }
@@ -29,4 +38,12 @@ internal class EvalExp : ExpAlg<Eval> {
     }
 
     override fun Eval.plus(value: Eval): Eval = add(this, value)
+}
+
+internal class EvalMul : EvalExp(), MulAlg<Eval> {
+    private fun multiply(a: Eval, b: Eval) = object : Eval {
+        override fun eval(): Int = a.eval() * b.eval()
+    }
+
+    override fun Eval.times(value: Eval): Eval = multiply(this, value)
 }
