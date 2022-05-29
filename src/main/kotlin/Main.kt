52a98c820cb8ffaf6f4beb398c95a7d387b74ea9
Christian Maus <c.maus@reply.de>
2022-05-29 10:47:26 +0200

    syntactic sugar: use fun interface for Eval

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -24,26 +24,20 @@ internal interface MulAlg<T> : ExpAlg<T> {
     operator fun T.times(value: T): T
 }
 
-internal interface Eval {
+internal fun interface Eval {
     fun eval(): Int
 }
 
 internal open class EvalExp : ExpAlg<Eval> {
-    override fun lit(n: Int): Eval = object : Eval {
-        override fun eval(): Int = n
-    }
+    override fun lit(n: Int): Eval = Eval { n }
 
-    private fun add(x: Eval, y: Eval): Eval = object : Eval {
-        override fun eval(): Int = x.eval() + y.eval()
-    }
+    private fun add(x: Eval, y: Eval): Eval = Eval { x.eval() + y.eval() }
 
     override fun Eval.plus(value: Eval): Eval = add(this, value)
 }
 
 internal class EvalMul : EvalExp(), MulAlg<Eval> {
-    private fun multiply(a: Eval, b: Eval) = object : Eval {
-        override fun eval(): Int = a.eval() * b.eval()
-    }
+    private fun multiply(a: Eval, b: Eval) = Eval { a.eval() * b.eval() }
 
     override fun Eval.times(value: Eval): Eval = multiply(this, value)
 }
