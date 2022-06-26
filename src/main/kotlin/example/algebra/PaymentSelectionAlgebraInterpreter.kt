0397f0683adb519fb5c54181c13cc4e1278331d6
Joshua von Mutius <j.vonmutius@reply.de>
2022-06-26 14:55:19 +0200

    Implement Object Algebra

--- src/main/kotlin/example/algebra/PaymentSelectionAlgebraInterpreter.kt
+++ src/main/kotlin/example/algebra/PaymentSelectionAlgebraInterpreter.kt
@@ -5,17 +5,20 @@ import example.model.*
 import example.service.CreditScoreService
 import example.service.PaymentService
 
-typealias EvalPaymentRule = (User) -> PaymentRestriction?
+typealias TBD = Any?
+// TODO: Step 1. Define typealiases to compile the tests
+typealias EvalPaymentRule = TBD
 
-typealias EvalRuleHead = (User) -> Boolean
+typealias EvalRuleHead = TBD
 
-typealias EvalCondition = (User) -> Boolean
+typealias EvalCondition = TBD
 
-typealias EvalUserCreditScore = (User) -> CreditScore
+typealias EvalUserCreditScore = TBD
 
-typealias EvalPaymentSelection = (User) -> PaymentRestriction
+typealias EvalPaymentSelection = TBD
 
 
+//TODO: Step 2. Implementation
 class PaymentSelectionAlgebraInterpreter(
     private val paymentService: PaymentService,
     private val creditScoreService: CreditScoreService
@@ -25,31 +28,25 @@ class PaymentSelectionAlgebraInterpreter(
     override val average = CreditScore.AVERAGE
     override val bad = CreditScore.BAD
 
-    override val `advance payment` = PaymentRestriction(AdvancePayment.nel())
-    override val `credit card payment` = PaymentRestriction(CreditCardPayment.nel())
+    override val `advance payment`: PaymentRestriction = TODO()
+    override val `credit card payment`: PaymentRestriction = TODO()
 
-    override val `credit score` = creditScoreService::creditScore
-    override val `credit card` = { user: User -> paymentService.creditCardType(user) != null }
+    override val `credit score`: EvalUserCreditScore = TODO()
+    override val `credit card`: EvalCondition = TODO()
 
-    override fun EvalUserCreditScore.`is worse than`(other: CreditScore) = { it: User -> this(it) < other }
+    override fun EvalUserCreditScore.`is worse than`(other: CreditScore): EvalCondition = TODO()
 
-    override fun EvalRuleHead.then(restriction: EvalPaymentSelection) = { it: User ->
-        if (this(it)) restriction(it) else null
-    }
+    override fun EvalRuleHead.then(restriction: EvalPaymentSelection): EvalPaymentRule = TODO()
 
-    override fun allow(paymentMethods: PaymentRestriction) = { _: User -> paymentMethods }
+    override fun allow(paymentMethods: PaymentRestriction): EvalPaymentSelection = TODO()
 
-    override fun PaymentRestriction.and(other: PaymentRestriction) = PaymentRestriction(this.value + other.value)
+    override fun PaymentRestriction.and(other: PaymentRestriction): PaymentRestriction = TODO()
 
-    override fun EvalPaymentRule.otherwise(restriction: EvalPaymentSelection) = { it: User ->
-        this(it) ?: restriction(it)
-    }
+    override fun EvalPaymentRule.otherwise(restriction: EvalPaymentSelection): EvalPaymentSelection = TODO()
 
-    override fun provided(condition: EvalCondition): EvalRuleHead = condition
+    override fun provided(condition: EvalCondition): EvalRuleHead = TODO()
 
-    override fun EvalPaymentRule.or(next: EvalPaymentRule): EvalPaymentRule = { it: User ->
-        this(it) ?: next(it)
-    }
+    override fun EvalPaymentRule.or(next: EvalPaymentRule): EvalPaymentRule = TODO()
 }
 
 
